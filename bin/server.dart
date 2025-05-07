ec37131684df03cea26adfc609233e090a42b144
Chima Precious <ositachima.co@gmail.com>
2025-05-07 19:27:32 +0000

    add working example

---
+++ bin/server.dart
@@ -0,0 +1,68 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:globe_ai/globe_ai.dart';
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart';
+import 'package:shelf_router/shelf_router.dart';
+
+T safeParseJson<T>(String jsonString) {
+  try {
+    return jsonDecode(jsonString) as T;
+  } catch (e) {
+    rethrow;
+  }
+}
+
+final _router = Router()
+  ..post('/generateText', _generateText)
+  ..post('/streamText', _streamText);
+
+Future<Response> _generateText(Request req) async {
+  final body = safeParseJson<Map<dynamic, dynamic>>(await req.readAsString());
+
+  final prompt = body['prompt'] as String;
+  final model = body['model'] as String;
+  final user = body['user'] as String?;
+
+  final result = await generateText(
+    model: openai.chat(model, user: user),
+    prompt: prompt,
+  );
+
+  return Response.ok(jsonEncode(result), headers: {
+    HttpHeaders.contentTypeHeader: ContentType.json.mimeType,
+  });
+}
+
+Future<Response> _streamText(Request req) async {
+  final body = safeParseJson<Map<dynamic, dynamic>>(await req.readAsString());
+
+  final prompt = body['prompt'] as String;
+  final model = body['model'] as String;
+  final user = body['user'] as String?;
+
+  final result = streamText(
+    model: openai.chat(model, user: user),
+    prompt: prompt,
+  ).map(utf8.encode);
+
+  return Response.ok(
+    result,
+    headers: {'Content-Type': 'text/plain'},
+  );
+}
+
+void main(List<String> args) async {
+  // Use any available host or container IP (usually `0.0.0.0`).
+  final ip = InternetAddress.anyIPv4;
+
+  // Configure a pipeline that logs requests.
+  final handler =
+      Pipeline().addMiddleware(logRequests()).addHandler(_router.call);
+
+  // For running in containers, we respect the PORT environment variable.
+  final port = int.parse(Platform.environment['PORT'] ?? '8080');
+  final server = await serve(handler, ip, port);
+  print('Server listening on port ${server.port}');
+}
