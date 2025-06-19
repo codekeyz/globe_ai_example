144b6ed1f16e931fae29b273d4d8f5041289c17a
Chima Precious <ositachima.co@gmail.com>
2025-06-19 00:51:06 +0000

    _

--- bin/server.dart
+++ bin/server.dart
@@ -53,11 +53,11 @@ Future<Response> _streamText(Request req) async {
 void main(List<String> args) async {
   final ip = InternetAddress.anyIPv4;
 
-  final handler =
-      Pipeline().addMiddleware(logRequests()).addHandler(_router.call);
+  final handler = Pipeline().addHandler(_router.call);
 
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
   final server = await serve(handler, ip, port);
+
   print('Server listening on port ${server.port}');
 }
 
