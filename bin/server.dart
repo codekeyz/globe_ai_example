a8b98806d73608b650fae80f128cc7979a123175
Chima Precious <ositachima.co@gmail.com>
2025-06-30 13:30:45 +0000

    _

--- bin/server.dart
+++ bin/server.dart
@@ -7,7 +7,7 @@ import 'package:shelf/shelf_io.dart';
 import 'package:shelf_router/shelf_router.dart';
 
 final _router = Router()
-  ..get('/', (req) => Response.ok('Welcome to the Globe AI Example Server!'))
+  ..get('/', (req) => Response.ok('Welcome to the Globe AI Example Server! ✅'))
   ..post('/generateText', _generateText)
   ..post('/streamText', _streamText);
 
